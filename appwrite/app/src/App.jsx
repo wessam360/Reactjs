089edf1da54e053e3be04af1d72f4fa7136c6eff
wessam aftab <wisamkayani360@gmail.com>
2024-08-18 16:14:56 +0500

    environment configured
    i have added config file to add environment variable and handle them in better way for production

--- appwrite/app/src/App.jsx
+++ appwrite/app/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import conf from './conf/conf'
 function App() {
-
+console.log(conf.APPWRITE_BUCKET_ID);
   return (
     <>
     <div>BLOG PROJECT WITH APPWRITE</div>
